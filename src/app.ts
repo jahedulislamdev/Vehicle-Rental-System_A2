fb05afee1c9dfe06cdeb4c7329228c318eb42113
jishan <jahedulislamdev@gmail.com>
2025-12-06 12:59:38 +0600

    fix: db check error

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
+import initDb from "./config/db";
 
 const app = express();
 // middleware
 app.use(express.json());
+
+initDb();
 app.get("/", (req: Request, res: Response) => {
     res.send("Vehicle Rental System is Running...");
 });
--- src/config/db.ts
+++ src/config/db.ts
@@ -10,7 +10,7 @@ const initDb = async () => {
          type VARCHAR(30) NOT NULL CHECK (type IN ('car','bike','van','SUV')),
          registration_number VARCHAR(20) UNIQUE NOT NULL,
          daily_rent_price INT NOT NULL CHECK(daily_rent_price > 0),
-         availability_status CHECK(availability_status IN ('available', 'booked'))
+         availability_status VARCHAR(20) CHECK(availability_status IN ('available', 'booked'))
       )`);
     await pool.query(` 
          CREATE TABLE IF NOT EXISTS users( 
@@ -19,7 +19,7 @@ const initDb = async () => {
          email VARCHAR(50) UNIQUE NOT NULL CHECK(email = LOWER(email)),
          password VARCHAR(15) CHECK(char_length(password) >=6),
          phone VARCHAR(14) NOT NULL,
-         role CHECK(role IN ('admin','customer'))
+         role VARCHAR(20) CHECK(role IN ('admin','customer'))
       )`);
     await pool.query(`
          CREATE TABLE IF NOT EXISTS bookings( 
@@ -28,8 +28,8 @@ const initDb = async () => {
          vehicle_id INT REFERENCES vehicles(id) ON DELETE CASCADE,
          rent_start_date DATE NOT NULL,
          rent_end_date DATE NOT NULL CHECK (rent_end_date >= rent_start_date),
-         total_price INT NOT NULL CHECK(total_price > 0)
-         status CHECK(status IN ('active', 'cancelled', 'returned'))
+         total_price INT NOT NULL CHECK(total_price > 0),
+         status VARCHAR(20) CHECK(status IN ('active', 'cancelled', 'returned'))
       )`);
 };
 
